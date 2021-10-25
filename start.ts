d56b58683571966e5cf1e79f36b1d52c7550b4fd
David Hines <david.hines.01@gmail.com>
2021-10-25 18:14:28 -0300

    Added logger to start.ts

--- start.ts
+++ start.ts
@@ -1,6 +1,7 @@
 import app from  './server';
+import logger from './src/lib/logger';
 
 const port = process.env.PORT || 3000;
 app.listen(port);
 // tslint:disable-next-line:no-console
-console.log('Contacts Api server started on: ' + port);
+logger.debug(`Contacts Api server started on: ${port}`);
